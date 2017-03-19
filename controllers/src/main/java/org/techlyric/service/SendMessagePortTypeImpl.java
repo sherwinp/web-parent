09bc6eaad2a73930de14c7f237615d518828e492
Sherwin Price <sherwin.price@gmail.com>
2017-03-19 08:07:49 -0400

    add web service endpoint in sub url.

--- controllers/src/main/java/org/techlyric/service/SendMessagePortTypeImpl.java
+++ controllers/src/main/java/org/techlyric/service/SendMessagePortTypeImpl.java
@@ -1,9 +1,20 @@
 package org.techlyric.service;
  
+import javax.annotation.Resource;
 import javax.jws.WebService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
  
 @WebService
 public class SendMessagePortTypeImpl implements SendMessagePortType {
+	
+	@Resource
+	private WebServiceContext context;
+	//MessageContext ctx = context.getMessageContext();
+	//HttpServletRequest request = (HttpServletRequest)ctx.get(AbstractHTTPDestination.HTTP_REQUEST);
+	//HttpServletResponse response = (HttpServletResponse)    ctx.get(AbstractHTTPDestination.HTTP_RESPONSE);
 	@Override
 	public SendMessagePortResponse SendMessage(ExtractionMessage extract){
 		return new SendMessagePortResponse();
