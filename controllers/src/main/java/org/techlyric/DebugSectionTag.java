640648d2a958e1bf44378511cc1c0c0426e09f47
Sherwin Price <sherwin.price@gmail.com>
2017-04-07 06:17:41 -0400

    debug tag for request attributes

--- controllers/src/main/java/org/techlyric/DebugSectionTag.java
+++ controllers/src/main/java/org/techlyric/DebugSectionTag.java
@@ -8,14 +8,33 @@ import java.io.*;
 
 public class DebugSectionTag extends SimpleTagSupport {
 	public void doTag() throws JspException, IOException {
-		JspWriter out = getJspContext().getOut();
 		
-		PageContext pageContext = (PageContext) getJspContext();  
-		HttpServletRequest request = (HttpServletRequest) pageContext.getRequest(); 
-		HttpSession session = (HttpSession) pageContext.getSession(); 
-		java.util.Enumeration<String> reqEnum = session.getAttributeNames();
-		while (reqEnum.hasMoreElements()) {
-			String s = reqEnum.nextElement();
+
+		PageContext pageContext = (PageContext) getJspContext();
+
+		HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
+		formatTag(request, request.getAttributeNames());
+		if (request.getSession(false) != null) {
+			try {
+				formatTag( request.getSession(false), request.getSession(false).getAttributeNames());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	private void formatTag(HttpServletRequest request,java.util.Enumeration<String> attrEnums) throws JspException, IOException {
+		JspWriter out = getJspContext().getOut();
+		while (attrEnums.hasMoreElements()) {
+			String s = attrEnums.nextElement();
+			out.print(s);
+			out.println("==" + request.getAttribute(s));
+			out.println("<br />");
+		}
+	}
+	private void formatTag(HttpSession session,java.util.Enumeration<String> attrEnums) throws JspException, IOException {
+		JspWriter out = getJspContext().getOut();
+		while (attrEnums.hasMoreElements()) {
+			String s = attrEnums.nextElement();
 			out.print(s);
 			out.println("==" + session.getAttribute(s));
 			out.println("<br />");
