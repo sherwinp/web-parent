addbb944b72a9f2ca9fdca1ae349be8a625c73cc
sherwinp <sherwinprice@hotmail.com>
2017-04-16 16:53:29 -0400

    add policy

--- controllers/src/main/java/org/techlyric/service/SendMessagePortTypeImpl.java
+++ controllers/src/main/java/org/techlyric/service/SendMessagePortTypeImpl.java
@@ -8,11 +8,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.xml.ws.WebServiceContext;
 import javax.xml.ws.handler.MessageContext;
 
+import weblogic.jws.Policies;
+import weblogic.jws.Policy;
+
 @WebService(
 	portName="SendMessagePortType",
 	serviceName = "sendmessage",
 	targetNamespace="http://org.techlyric",
 	endpointInterface="org.techlyric.service.SendMessagePortType")
+@Policy(uri="Wssp1.2-2007-Wss1.1-X509-Basic256.xml")
 public class SendMessagePortTypeImpl implements SendMessagePortType {
 	
 	@Resource
